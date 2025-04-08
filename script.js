ebf0c23d920c9f4210ebab15894dc1881534ae30
pedroplatner <pedro.platner6@gmail.com>
2025-04-08 14:24:38 -0300

    scrollreveal

--- script.js
+++ script.js
@@ -4,10 +4,40 @@ const mobileMenu = document.getElementById("mobile_menu");
 mobileBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("show");
 
-  // Alterna o ícone
   if (mobileMenu.classList.contains("show")) {
     mobileBtn.textContent = "✖";
   } else {
     mobileBtn.textContent = "☰";
   }
 });
+
+
+ScrollReveal().reveal('.home', {
+    origin: 'top',
+    duration: 2000,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('.about', {
+    origin: 'right',
+    duration: 2000,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('.cartoes', {
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+})
+
+ScrollReveal().reveal('.dowload', {
+    origin: 'right',
+    duration: 2000,
+    distance: '20%'
+})
+
+ScrollReveal().reveal('.blog',{
+    origin: 'left',
+    duration: 2000,
+    distance: '20%'
+})
